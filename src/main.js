edb7eed1b844ca106a77817f2f594acbeb262bc1
o-matheus <matheusaguiar028@gmail.com>
2025-08-31 11:50:30 -0300

    chore: finalização da lógica de programação e organização do layout mobile.

--- src/main.js
+++ src/main.js
@@ -1,27 +1,72 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let atividadesPendentes = []
-    let atividadesCompletas = []
+    const atividadesPendentes = []
 
-    const input = document.getElementById('input__compra')
-    const botao = document.getElementById('adicionar__item')
-    const textoCta = document.querySelector('.texto__cta')
+    const input = document.getElementById('input__compra');
+    const botaoAdicionar = document.getElementById('adicionar__item');
+    const textoCta = document.querySelector('.texto__cta');
+    const lista = document.querySelector('.lista');
 
 
 
-    botao.addEventListener('click', (e) => {
+
+    botaoAdicionar.addEventListener('click', (e) => {
         e.preventDefault();
         adicionarItem();
-        if (atividadesPendentes.length > 0 || atividadesCompletas.length > 0) {
-            textoCta.style.display = 'none';
-        }
+        criarLista();
+    })
+
+    lista.addEventListener('click', (e) => {
+        apagarItem(e);
+        itemComprado(e);
     })
 
 
+
     function adicionarItem() {
-        const inputValue = input.value
-        atividadesPendentes.push(inputValue)
+        const inputValue = input.value;
+        atividadesPendentes.push(inputValue);
         input.value = '';
-        console.log(atividadesPendentes)
+        textoCta.style.display = 'none';
+
+    }
+
+    function criarLista() {
+        lista.innerHTML = '';
+        atividadesPendentes.forEach((item, index) => {
+            const li = document.createElement('li');
+            li.className = 'lista__item';
+            li.innerHTML =
+                `
+            <div class="info__left">
+                <input type="checkbox" class="checkbox__atividade" >
+                <span class="texto__lista">${item}</span>
+            </div>
+            <button type="button" class="btn__excluir" data-index= "${index}" title="excluir">
+                🗑️
+            </button>        
+        `;
+            lista.appendChild(li);
+        })
+    }
+
+    function apagarItem(e) {
+        const botaoApagar = e.target.classList.contains('btn__excluir')
+        if (botaoApagar) {
+            const index = e.target.getAttribute('data-index');
+            atividadesPendentes.splice(index, 1);
+            criarLista();
+            if (atividadesPendentes.length == 0) {
+                textoCta.style.display = 'block';
+            }
+        }
+    }
+
+    function itemComprado(e) {
+        const checkbox = e.target.classList.contains('checkbox__atividade')
+        if(checkbox) {
+            const span = e.target.nextElementSibling;
+            span.classList.toggle('atividade__completa')
+        }
     }
 })
 
